7b1da018dd97d5adcf59aadc6d0f6ab9739966d1
Jorge Sanchez-Cano <jorgescmsd@gmail.com>
2019-02-28 12:17:53 +0000

    the script now changes the post after the window is loaded

--- site/public/scripts/script1.js
+++ site/public/scripts/script1.js
@@ -1,1 +1,18 @@
-var element = document.getElementById("username").innerHTML = "Jorge S-C ";
+"use strict"
+function changeUsername(username){
+  var element = document.getElementById("username");
+  element.innerHTML = username;
+}
+
+function loadPost(postData){
+  var ids = ["username","title","content-text"];
+  var element;
+  for(var i = 0; i < ids.length; i++){
+    var element = document.getElementById(ids[i]);
+    element.innerHTML = postData[i];
+  }
+}
+
+window.onload = function() {
+  loadPost(["Jorge S-C ","Go to el retiro","madrid is great!"]);
+}
