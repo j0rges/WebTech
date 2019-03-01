263a170b963d450d0f3d356150c9ebdaa4ac3716
Konstantina Psoma <kp16340@my.bristol.ac.uk>
2019-03-01 17:36:08 +0000

    Trying to draw data from db

--- site/public/scripts/script1.js
+++ site/public/scripts/script1.js
@@ -6,6 +6,7 @@ function changeUsername(username){
 
 function loadPost(postData){
   var ids = ["username","title","content-text"];
+  //var data = //write querry here.
   var element;
   for(var i = 0; i < ids.length; i++){
     var element = document.getElementById(ids[i]);
