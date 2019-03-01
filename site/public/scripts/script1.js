fad0cf327fddb7567d9e6f0bf716aeb4cc72972d
Jorge Sanchez-Cano <jorgescmsd@gmail.com>
2019-03-01 12:52:46 +0000

    when template 1 is loaded, the script loads resultsTemplate and appends it with some data to the colun of results.

--- site/public/scripts/script1.js
+++ site/public/scripts/script1.js
@@ -1,28 +1,39 @@
 "use strict"
 
-addEventListener('load',start);
-start(){
-  loadPost(["Jorge S-C ","Go to el retiro","madrid is great!"]);
+addEventListener('load',loadPosts);
+
+
+
+function loadPosts(){
+  // substitute for getting the actual data from the server/database.
+  var postData = ["Jorge S-C ","Go to el retiro","madrid is great!"];
+  // Get the template from the server.
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = receive;
+  req.open("Get","resultTemplate.html",true);
+  req.send();
+  function receive(){
+    if (this.readyState != XMLHttpRequest.DONE) return;
+    var template = this.responseText;
+    appendSearchResult(postData, template);
+  }
+
 }
 
-function changeUsername(username){
-  var element = document.getElementById("username");
-  element.innerHTML = username;
-}
-
-/*
-function loadPost(postData){
-  var ids = ["username","title","content-text"];
-  var element;
+// Given the data and the template append the corresponding post to the
+// search result area.
+function appendSearchResult(postData, template){
+  // Create the section and give it the appropiate class.
+  var result = document.createElement("section");
+  result.setAttribute("class", "search-result")
+  // put the template inside.
+  result.innerHTML = template;
+  // Load the post into the template.
+  var ids = ["#username","#title","#content-text"];
   for(var i = 0; i < ids.length; i++){
-    var element = document.getElementById(ids[i]);
+    var element = result.querySelector(ids[i]);
+    console.log(element);
     element.innerHTML = postData[i];
   }
-}
-*/
-
-function appendSearchResult(postData){
-  // Create the section and
-  var result = document.createElement("section");
-  result.setAttribute("class","search-result")
+  document.querySelector("#results-area").appendChild(result);
 }
