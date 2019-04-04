d6ac9f7b6781c27b45487f11a3c3598f6091e75c
Jorge Sanchez-Cano <jorgescmsd@gmail.com>
2019-04-04 11:18:55 +0100

    dealing with multipart, doesnt work for images

--- site/server.js
+++ site/server.js
@@ -86,8 +86,7 @@ async function handle(request, response) {
       }
     }
     else if (contentType == 'multipart/form-data'){
-      console.log(request.headers);
-      response.end('ok');
+      handleMultipart(request, response);
     }
     else {
       console.log(request.headers);
@@ -137,6 +136,69 @@ function handleFileRequest(request, response, url){
   let file = "./public" + url;
   fs.readFile(file, ready);
   function ready(err, content) { deliver(response, type, err, content); }
+}
+
+function handleMultipart(request, response) {
+
+  boundary = request.headers["content-type"].split(";")[1];
+  boundary = boundary.split("=")[1]
+
+  let body = '';
+  request.on('data', add);
+  function add(chunk) {body += chunk.toString();}
+  request.on('end', endStuff);
+  function endStuff(){
+    parts = parseMultipart(body);
+    fs.writeFile('image.jpg', parts[2].content, function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+    });
+    response.end('ok');
+  }
+}
+
+function parseMultipart(body){
+  parts = body.split(boundary);
+  indices = [];
+
+  for (var i in parts){
+    parts[i] = parseMultipartPiece(parts[i]);
+    if(parts[i].content == undefined){
+      // to adjust for the indices changing.
+      indices.push(i - indices.length);
+    }
+  }
+  //delete the elements that are empty:
+  for (var i = 0; i < indices.length; i++) {
+    parts.splice(indices[i],1);
+  }
+  return parts;
+}
+
+// Returns an object with the name of the form field as attribute field and
+// the content as attribute content.
+function parseMultipartPiece(piece){
+  let field = {filename: "", contentType: types["txt"], content: ""};
+  let temp;
+  temp = piece.substring(piece.indexOf("name=\"")+6);
+  field.name = temp.substring(0,temp.indexOf("\""));
+
+  let index = piece.indexOf("filename=\"");
+  // Parse a piece that carries a file.
+  if(index > 0){
+    temp = piece.substring(index+10);
+    field.filename = temp.substring(0,temp.indexOf("\""));
+    temp = piece.substring(piece.indexOf("Content-Type: ")+14);
+    field.contentType = temp.substring(0,temp.indexOf("\r\n"));
+    temp = temp.substring(temp.indexOf("\r\n")+2);
+    field.content = temp.substring(0,temp.lastIndexOf("\r\n"));
+  }
+  // Parse a piece that contains text.
+  else{
+    piece = piece.split("\r\n");
+    field.content = piece[3];
+  }
+  return field;
 }
 
 function getQuery(url) {
