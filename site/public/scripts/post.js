85fd473706dd87f135221ed37ed0fb4ac7a73451
Jorge Sanchez-Cano <jorgescmsd@gmail.com>
2019-05-02 15:35:08 +0100

    script for posting the image

---
+++ site/public/scripts/post.js
@@ -0,0 +1,24 @@
+"use strict"
+
+addEventListener('load',poststart);
+
+function poststart() {
+  //Should also put this for when submit is clicked. But checking that some
+  //image has actually been selected.
+  const b = document.querySelector("#loadButton");
+  b.addEventListener("click",sendImage);
+}
+
+function sendImage(){
+  var reader = new FileReader();
+  var file = document.getElementById('imageload').files[0];
+  var request = new XMLHttpRequest();
+  var url = "/put/" + file.name;
+  request.open("put",url);
+
+  reader.onload = function(event){
+    console.log("here");
+    request.send(event.target.result);
+  };
+  reader.readAsDataURL(file);
+}
