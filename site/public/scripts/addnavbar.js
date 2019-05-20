4d7e165e99e646163086128d4021d114edc7fada
Jorge Sanchez-Cano <jorgescmsd@gmail.com>
2019-05-20 10:50:58 +0100

    the navigation bar can be included in every index file by including addnvbar.js and navbarstyle.css to the head

---
+++ site/public/scripts/addnavbar.js
@@ -0,0 +1,22 @@
+"use strict";
+
+addNavBar();
+
+function addNavBar() {
+  var adrr = "/navbar.html"
+  function callback(t){
+    var navbar = document.createElement("nav");
+    navbar.innerHTML = t;
+    document.body.parentNode.insertBefore(navbar,document.body);
+  }
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = receive;
+  req.open("Get",adrr,true);
+  req.send();
+  var template;
+  function receive(){
+    if (this.readyState != XMLHttpRequest.DONE) return;
+    template = this.responseText;
+    callback(template);
+  }
+}
