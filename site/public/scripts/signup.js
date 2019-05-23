6a9eb32e53a95fb3034dfd0cc47f16249adb0c21
Konstantina Psoma <kp16340@my.bristol.ac.uk>
2019-05-23 01:02:07 +0100

    Trying to connect with client (not working version)

--- site/public/scripts/signup.js
+++ site/public/scripts/signup.js
@@ -4,11 +4,25 @@ function sendUser() {
   console.log("In function sendUser");
   var request = new XMLHttpRequest();
   request.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("signupform").innerHTML = this.responseText;
+    if (this.readyState == 4) {
+      if (this.status == 200){
+         //Change page to succesfully signed up
+         this.responseText
+      }
+      else {
+        //Get response
+      }
     }
   };
+  var bodysend = {
+    username: document.getElementById(),
+    email: ,
+    password: 
+  }
+  // Convert to json format
   var url = "/post/signup";
   request.open("POST", url);
-  request.send();
+  //request.send(body);
 }
+
+
