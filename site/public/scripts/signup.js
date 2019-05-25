84709574c26331923d31b57899b272c4b6d15569
Konstantina Psoma <kp16340@my.bristol.ac.uk>
2019-05-25 21:13:14 +0100

    small stuff

--- site/public/scripts/signup.js
+++ site/public/scripts/signup.js
@@ -2,31 +2,31 @@ Cookies.set("ieat", "true", {expires:1});
 
 var myCookie = Cookies.get("ieat");
 
-var title = document.querySelector("h1");
+var element = document.getElementById("title");
 
 if(myCookie){
-  console.log("cookie!")
+  element.innerHTML = "Cookies!"
 }
 
-function changeNavbar() {
-  // Change the navigation bar
-  getTemplate("../navbar.html",c);
-  function c(template){
-    var resultArea = document.querySelector("#results-area");
-    resultArea.innerHTML= template;
-    document.querySelector("#postLocation").value = pageDestination;
-  }
-}
+// function changeNavbar() {
+//   // Change the navigation bar
+//   getTemplate("../navbar.html",c);
+//   function c(template){
+//     var resultArea = document.querySelector("#results-area");
+//     resultArea.innerHTML= template;
+//     document.querySelector("#postLocation").value = pageDestination;
+//   }
+// }
 
-function getTemplate(adrr,callback) {
-  var req = new XMLHttpRequest();
-  req.onreadystatechange = receive;
-  req.open("Get",adrr,true);
-  req.send();
-  var template;
-  function receive(){
-    if (this.readyState != XMLHttpRequest.DONE) return;
-    template = this.responseText;
-    callback(template);
-  }
-}
+// function getTemplate(adrr,callback) {
+//   var req = new XMLHttpRequest();
+//   req.onreadystatechange = receive;
+//   req.open("Get",adrr,true);
+//   req.send();
+//   var template;
+//   function receive(){
+//     if (this.readyState != XMLHttpRequest.DONE) return;
+//     template = this.responseText;
+//     callback(template);
+//   }
+// }
